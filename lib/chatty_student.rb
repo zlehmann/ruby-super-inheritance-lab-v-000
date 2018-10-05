f3dc213fa07bb357e04f961c9515412add6abc7b
Zachary Lehmann <zlehmann@gmail.com>
2018-10-05 20:58:44 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,14 +2,14 @@ class ChattyStudent < Student
 
   def hello
     super
-    puts "Hey there! I'm so excited to learn stuff.\n\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end
 
   def raise_hand
     super
     i=1
     while i <= 10
-      puts "Pick me!\n\n"
+      puts "Pick me!\n"
       i += 1
     end
   end
